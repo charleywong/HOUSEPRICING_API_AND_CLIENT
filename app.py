08a93a71a3e40d8a57b44a82730e139c3ef143f1
Christine Bui <christine.tvbui@gmail.com>
2018-10-02 12:31:24 +1000

    flask and react merge

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, send_from_directory
 from flask_restplus import Resource, Api
 
 from api import blueprint
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='templates/static')
 
 @app.route('/')
 def index():
     return render_template("index.html")
-    
+    #return send_from_directory('./build','index.html');
+
 app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
