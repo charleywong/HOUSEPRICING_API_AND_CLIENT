efa197df16272ee57c6a8169db36a71eb95731c4
200sx <g.ontri@student.unsw.edu.au>
2018-10-16 18:43:24 +1100

    added register

--- api.py
+++ api.py
@@ -1,16 +1,95 @@
 from flask import Blueprint
-from flask_restplus import Resource, Api
+from flask import Flask, request
+from flask_restplus import Api, Resource, fields, model, reqparse
+import pymongo
+from pymongo import MongoClient
+import json
+from bson import json_util
 
 # API AND SWAGGER INIT
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint, title='Our Api')
 
-# API ROUTES HERE?
-@api.route('/test')
-class HelloWorld(Resource):
-    def get(self):
-        return { 'some': 'shit right here' }, 200
+usr_details = {
+    "_id": "ass2.gontri",
+    "user": "gontri",
+    "db": "ass2",
+    "roles": [
+        {
+            "role": "dbOwner",
+            "db": "ass2"
+        }
+    ]
+}
 
-# api call from react front end:
-    # XMLHttpRequest()
-    # 
+
+user_model = api.model('User Login Data', {
+'name': fields.String,
+'password': fields.String
+})
+
+
+host='mongodb://gontri:Lolcats123@ds153552.mlab.com:53552/ass2'
+
+#to do calls; do it as such
+    #collection = db[arg["indicator_id"]]
+    #collection.insert_one(json.loads(json_util.dumps(new_data)))
+
+client = MongoClient(host)
+db = client['ass2']
+
+
+
+# API ROUTES HERE?
+@api.route('/register')
+class register(Resource):
+    @api.expect(user_model, validate = True)
+
+    @api.response(201, 'Successfully created new user')
+    @api.response(400, 'Invalid login details (empty fields) or user already found')
+    def post(self):
+        
+        arg = request.json
+
+        #validity check for payload
+        if "name" not in arg.keys():
+            return {"message": "Improper payload format - no proper username"}, 400
+
+        if "password" not in arg.keys():
+            return {"message": "Improper payload format - no proper password"}, 400
+
+        if (arg["name"] == "") or (arg["password"] == ""):
+            return {"message": "Provide non-empty user/pass"}, 400
+
+        #check user does not exist already
+        collection = db["users"]
+        for document in collection.find():
+            if document["name"] == arg["name"]:
+                return {"message": "username already exists"}, 400
+
+
+
+        new_data = user_model
+
+        new_data["name"] = arg["name"]
+        new_data["password"] = arg["password"]
+
+        
+        collection.insert_one(json.loads(json_util.dumps(new_data)))
+
+        return { 'message': 'created' }, 201
+
+@api.route('/login')
+class login(Resource):
+    @api.expect(user_model, validate = True)
+
+    @api.response(200, 'Successfully found')
+    @api.response(404, 'User not found.')
+    @api.response(400, 'Invalid login details (empty fields)')
+    def post(self):
+        new_data = user_info
+        new_data["name"] = "Cat"
+        new_data["password"] = "Tuong"
+        collection = db["users"]
+        collection.insert_one(json.loads(json_util.dumps(new_data)))
+        return { 'some': 'shit right here' }, 200
