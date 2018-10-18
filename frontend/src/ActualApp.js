508adb5017e5ef8e86b22b55d9ee4e025f2c31c8
Christine Bui <christine.tvbui@gmail.com>
2018-10-18 16:46:22 +1100

    sell form complete

--- frontend/src/ActualApp.js
+++ frontend/src/ActualApp.js
@@ -1,32 +1,99 @@
 import React, { Component } from 'react';
-import { Button, Form, Divider } from 'semantic-ui-react';
+import { Button, Form, Divider, Tab } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-
 import { DateInput } from 'semantic-ui-calendar-react';
-
-import './ActualApp.css';
-
-import bannerimg from './img/bg2.jpg';
-
 import createHistory from "history/createBrowserHistory";
 
+import './ActualApp.css';
+import bannerimg from './img/bg2.jpg';
+
+const history = createHistory({forceRefresh:true})
 const Title2 = ({ text }) => ( <h1 class='App-title2'>{text}</h1> );
 const Title3 = ({ text }) => ( <h2 class='App-title3'>{text}</h2> );
-
 const bannerStyle = {
   height:300,
   backgroundImage: 'url(' + bannerimg + ')',
   backgroundSize: 'cover'
 };
-
 const Banner = ({ img, text }) => (
   <div style={bannerStyle}>
   </div>
 );
 
-const history = createHistory({forceRefresh:true})
+const optionsType = [
+  { key: 'H', text: 'House', value:'h'},
+  { key: 'U', text: 'Unit', value:'u'},
+  { key: 'T', text: 'Townhouse', value:'t'}
+]
 
+const optionsNums = [
+  {key: '1', text: '1', value:'1'},
+  {key: '2', text: '2', value:'2'},
+  {key: '3', text: '3', value:'3'},
+  {key: '4', text: '4', value:'4'},
+  {key: '5', text: '5', value:'5'},
+  {key: '6', text: '6', value:'6'},
+  {key: '7', text: '7', value:'7'},
+  {key: '8', text: '8', value:'8'},
+  {key: '9', text: '9', value:'9'},
+  {key: '10', text: '10', value:'10'},
+]
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+class ActualApp extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <div class='ui main text container'>
+          <Title2 text='PROPERTY VALUATION API'/>
+          <Title3 text='Overview' />
+          <h4>User Inputs</h4>
+          <p>Users are required to enter as much information about the target property as possible in order to produce accurate predictions about the housing market. Fields are as follows:</p>
+          <h4>Output</h4>
+          <p>The service will run your data through our machine learning model to give an accurate property valuation.</p>
+          <Link to='/docs'><button class='ui button'>View detailed usage instructions</button></Link><br /><br /><br />
+          <ApiForm location={this.props.location}/>
+
+        </div>
+      </div>
+    );
+  }
+};
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
 class ApiForm extends React.Component {
+
+  render() {
+
+    const panes = [
+      {menuItem: 'Buy Property', render: () => <Tab.Pane><Buy /></Tab.Pane>},
+      {menuItem: 'Sell Property', render: () => <Tab.Pane><Sell />
+      </Tab.Pane>},
+      {menuItem: 'Renovate Property', render: () => <Tab.Pane><Renovate /></Tab.Pane>},
+    ]
+
+    return(
+      <div style={{marginBottom:50}}>
+        <Tab menu={{ secondary: true, pointing: true, attached:true }} panes={panes} />
+      </div>
+    );
+  }
+};
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+class Sell extends Component {
   constructor(props) {
     super(props);
     this.location = this.props.location;
@@ -98,154 +165,113 @@ class ApiForm extends React.Component {
     history.push('/result', this.state)
 
   }
-  render() {
-    return(
+  render () {
+    return (
       <div>
-      <Title2 text='Begin Valuation' />
       <Form name='apiform' id='apiform'
         onSubmit={this.handleSubmit}
-        class='ui form'
-        style={{marginBottom:60}}>
+        class='ui form'>
+      <Title3 text='Location & Date' />
+        <Form.Group widths='equal'>
+          <Form.Input required fluid width={9}
+            name='address'
+            value={this.state.address}
+            onChange={this.handleChange}
+            label='Address'
+            placeholder='Sydney' />
+          <Form.Input fluid width={4}
+            name='suburb'
+            value={this.state.suburb}
+            onChange={this.handleChange}
+            label='Suburb'
+            placeholder='Sydney' />
+          <Form.Input fluid width={3}
+            name='postcode'
+            value={this.state.postcode}
+            onChange={this.handleChange}
+            label='Postcode'
+            placeholder='2000' />
+        </Form.Group>
+          <DateInput
+          label='Date'
+          name="date"
+          placeholder="Date"
+          value={this.state.date}
+          onChange={this.handleChangeDate} />
 
-        <Title3 text='Location & Date' />
-          <Form.Group widths='equal'>
-            <Form.Input required fluid width={9}
-              name='address'
-              value={this.state.address}
-              onChange={this.handleChange}
-              label='Address'
-              placeholder='Sydney' />
-            <Form.Input fluid width={4}
-              name='suburb'
-              value={this.state.suburb}
-              onChange={this.handleChange}
-              label='Suburb'
-              placeholder='Sydney' />
-            <Form.Input fluid width={3}
-              name='postcode'
-              value={this.state.postcode}
-              onChange={this.handleChange}
-              label='Postcode'
-              placeholder='2000' />
-          </Form.Group>
-            <DateInput
-            label='Date'
-            name="date"
-            placeholder="Date"
-            value={this.state.date}
-            onChange={this.handleChangeDate} />
-
-          <Divider />
-          <Title3 text='Property Features' />
-          <Form.Select label='Type'
-            name='type'
-            options={optionsType}
-            value={this.state.type}
-            onChange={this.handleChangeSelect}
-            placeholder='Type of Property' />
-          <Form.Group widths='equal'>
-            <Form.Select label='Bedrooms'
-              name='bedrooms'
-              onChange={this.handleChangeSelect}
-              options={optionsNums}
-              placeholder='Number of Bedrooms'/>
-            <Form.Select label='Bathrooms'
-              name='bathrooms'
-              onChange={this.handleChangeSelect}
-              options={optionsNums}
-              placeholder='Number of Bathrooms'/>
-            <Form.Select label='Car Spaces'
-              name='carslots'
-              onChange={this.handleChangeSelect}
-              options={optionsNums}
-              placeholder='Number of Car Spaces'/>
-          </Form.Group>
-          <Form.Group widths='equal'>
-            <Form.Input fluid
-              name='landsize'
-              onChange={this.handleChange}
-              value={this.state.landsize}
-              label='Landsize (m^2)'
-              placeholder='399'/>
-            <Form.Input fluid
-              name='buildingarea'
-              onChange={this.handleChange}
-              value={this.state.buildingarea}
-              label='Building Area (m^2)'
-              placeholder='399' />
-          </Form.Group>
         <Divider />
+        <Title3 text='Property Features' />
+        <Form.Select label='Type'
+          name='type'
+          options={optionsType}
+          value={this.state.type}
+          onChange={this.handleChangeSelect}
+          placeholder='Type of Property' />
+        <Form.Group widths='equal'>
+          <Form.Select label='Bedrooms'
+            name='bedrooms'
+            onChange={this.handleChangeSelect}
+            options={optionsNums}
+            placeholder='Number of Bedrooms'/>
+          <Form.Select label='Bathrooms'
+            name='bathrooms'
+            onChange={this.handleChangeSelect}
+            options={optionsNums}
+            placeholder='Number of Bathrooms'/>
+          <Form.Select label='Car Spaces'
+            name='carslots'
+            onChange={this.handleChangeSelect}
+            options={optionsNums}
+            placeholder='Number of Car Spaces'/>
+        </Form.Group>
+        <Form.Group widths='equal'>
+          <Form.Input fluid
+            name='landsize'
+            onChange={this.handleChange}
+            value={this.state.landsize}
+            label='Landsize (m^2)'
+            placeholder='399'/>
+          <Form.Input fluid
+            name='buildingarea'
+            onChange={this.handleChange}
+            value={this.state.buildingarea}
+            label='Building Area (m^2)'
+            placeholder='399' />
+        </Form.Group>
+      <Divider />
 
-        <Button
-          type='submit'
-          content='Submit'
-          icon='right arrow'
-          labelPosition='left'
-          role='button'>
-        </Button>
+      <Button
+        type='submit'
+        content='Submit'
+        icon='right arrow'
+        labelPosition='left'
+        role='button'>
+      </Button>
       </Form>
+
       </div>
     );
   }
-};
-
-// LAT AND LONG FORM FIELDS
-// <Form.Group widths='equal'>
-//   <Form.Input fluid
-//     name='latitude'
-//     id='latitude'
-//     value={this.state.latitude}
-//     onChange={this.handleChange}
-//     label='Latitude'
-//     placeholder='' />
-//   <Form.Input fluid
-//     name='longitude'
-//     id='longitude'
-//     value={this.state.longitude}
-//     onChange={this.handleChange}
-//     label='Longitude'
-//     placeholder='' />
-// </Form.Group>
-
-const optionsType = [
-  { key: 'H', text: 'House', value:'h'},
-  { key: 'U', text: 'Unit', value:'u'},
-  { key: 'T', text: 'Townhouse', value:'t'}
-]
-
-const optionsNums = [
-  {key: '1', text: '1', value:'1'},
-  {key: '2', text: '2', value:'2'},
-  {key: '3', text: '3', value:'3'},
-  {key: '4', text: '4', value:'4'},
-  {key: '5', text: '5', value:'5'},
-  {key: '6', text: '6', value:'6'},
-  {key: '7', text: '7', value:'7'},
-  {key: '8', text: '8', value:'8'},
-  {key: '9', text: '9', value:'9'},
-  {key: '10', text: '10', value:'10'},
-]
-
-class ActualApp extends Component {
+}
+/******************************************************************************/
+class Buy extends Component {
   render() {
     return (
       <div>
-        <Banner />
-        <div class='ui main text container'>
-          <Title2 text='PROPERTY VALUATION API'/>
-          <Title3 text='Overview' />
-          <h4>User Inputs</h4>
-          <p>Users are required to enter as much information about the target property as possible in order to produce accurate predictions about the housing market. Fields are as follows:</p>
-          <h4>Output</h4>
-          <p>The service will run your data through our machine learning model to give an accurate property valuation.</p>
-          <Link to='/docs'><button class='ui button'>View detailed usage instructions</button></Link><br /><br /><br />
-          <div class='ui divider' />
-          <ApiForm location={this.props.location}/>
-
-        </div>
+      Buy
       </div>
     );
   }
-};
+}
+/******************************************************************************/
+class Renovate extends Component {
+  render() {
+    return (
+      <div>
+      Renovate
+      </div>
+    );
+  }
+}
 
 export default ActualApp;
